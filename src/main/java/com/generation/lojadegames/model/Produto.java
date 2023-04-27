803555e70bb66ecbf54cb4363ead74f4887060f3
juliana-inocencio <jinocencio0911@gmail.com>
2023-04-27 15:55:18 -0300

    Loja de Games

--- src/main/java/com/generation/lojadegames/model/Produto.java
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -43,6 +43,11 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+	
 
 	public Long getId() {
 		return id;
@@ -83,5 +88,15 @@ public class Produto {
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
 	
 }
